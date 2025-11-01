4876d8422d310e4424278c1c4bd13e78db394b2c
Amal k <119653788+Amalkxx02@users.noreply.github.com>
2025-11-01 18:46:16 +0530

    fixed file name mismatch

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from crude import product_crude, file_crude
+from crud import product_crud, file_crud
 
 # Initialize the main FastAPI application
 # This acts as the entry point for all routes defined in submodules.
@@ -14,8 +14,8 @@ app = FastAPI(
 
 # Include the router responsible for product-related CRUD operations.
 # Handles product creation, retrieval, updating, and deletion.
-app.include_router(product_crude.router)
+app.include_router(product_crud.router)
 
 # Include the router responsible for handling file-related logic.
 # Deals with file uploads, metadata management, and related operations.
-app.include_router(file_crude.router)
+app.include_router(file_crud.router)
